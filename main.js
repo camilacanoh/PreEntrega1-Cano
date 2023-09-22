6a406cbf129671c26cbc5fda7a3b777b00c03012
Camila Cano <canohcj@gmail.com>
2023-09-21 22:10:16 -0300

    Pre-Entrega2

--- main.js
+++ main.js
@@ -1,77 +1,51 @@
-const productos = [
-  {nombre:"Remera", precio: 1000},
-  {nombre:"Pantalon", precio: 2000},
-  {nombre:"Buzo", precio: 3000},
-  {nombre:"Vestido", precio: 2500},
-  {nombre:"Mochila", precio: 4000},
-  {nombre:"Medias", precio: 200},
-  {nombre:"Campera", precio: 15000}
-  ]
+let carrito =[];
 
-  let carrito=[]
+const inventario = [
+  { nombre: "Remera", precio: 500},
+  { nombre: "Buzo", precio: 1000},
+  { nombre: "Vestido", precio: 1500},
+  { nombre: "Mochila", precio: 2000},
+  { nombre: "Medias", precio: 100},
+];
 
-  let seleccion = prompt ("Hola, desea comprar algo?")
-
-  while(seleccion != "si" && seleccion !="no"){
-    alert ("Por favor ingresa si o no")
-    seleccion = prompt ("Hola, desea comprar algo?")
-  }
-
-  if(seleccion ==="si"){
-    alert("Nuestra lista de productos")
-    let todosLosProductos = productos.map ((producto)=>producto.nombre + " " + producto.precio + "$" );
-    alert (todosLosProductos.join(" - "))
-} else if (seleccion==="no"){
-    alert("¡Gracias por visitarnos, hasta pronto!");
+function agregarAlCarrito(producto) {
+  carrito.push(producto);
 }
 
-while (seleccion==="si"){
-  let producto = prompt ("Agrega un producto a tu carrito")
-  let precio = 0
+function mostrarInventario() {
+  const inventarioList = document.getElementById("Inventario");
+  inventarioList.innerHTML = "";
 
-  if(producto==="Remera" || producto=== "Pantalon" || producto=== "Buzo" || producto==="Vestido" || producto==="Mochila" || producto==="Medias" || producto==="Campera"){
-    switch (producto){
-      case "Buzo":
-        precio= 3000;
-        break;
-      case "Campera":
-        precio= 15000;
-        break;
-      case "Medias":
-        precio= 200;
-        break;
-      case "Mochila":
-        precio= 4000;
-        break;
-      case "Pantalon":
-        precio=2000;
-        break;
-      case "Remera":
-        precio=1000;
-        break;
-      case "Vestido":
-        precio=2500;
-        break;
-      default:
-        break;
-  }
-    let unidades = parseInt(prompt ("¿Cúantas unidades quiere llevar?"))
-    carrito.push({producto, unidades, precio})
-  } else{
-    alert("Producto inexistente. Intente nuevamente")
+  inventario.forEach((producto) => {
+    const item = document.createElement("li");
+    item.textContent = `${producto.nombre} - $${producto.precio}`;
+    inventarioList.appendChild(item);
+  });
 }
 
-seleccion = prompt("¿Desea agregar otro producto?")
-
-while(seleccion==="no"){
-  alert("Gracias por tu compra! Hasta pronto!")
-    carrito.forEach((carritoFinal)=>{
-    console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar por producto ${carritoFinal.unidades * carritoFinal.precio}`
-    )
-    })
-    break;
-  }
+function mostrarDinero(disponible) {
+  const dineroSpan = document.querySelector("#dinero span");
+  dineroSpan.textContent = `$${disponible}`;
 }
 
-const total = carrito.reduce ((acc, el) => acc + el.precio * el.unidades, 0)
-console.log(`El total a pagar es: ${total}`)
+mostrarInventario();
+mostrarDinero(10000); 
+
+
+const items = document.querySelectorAll("li");
+items.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    if (carrito.length < inventario.length) {
+      agregarAlCarrito(inventario[index]);
+      mostrarInventario();
+      mostrarDinero(10000 - calcularTotalCarrito());
+    } else {
+      alert("Ya tienes todos los productos en el carrito.");
+    }
+  });
+});
+
+
+function calcularTotalCarrito() {
+  return carrito.reduce((total, producto) => total + producto.precio, 0);
+}
